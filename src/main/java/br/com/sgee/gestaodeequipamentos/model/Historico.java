50108131b4ac76f67598422946573573c7878cc0
anderson.lima <andersonmlima2@gmail.com>
2025-04-28 15:47:56 -0300

    feat: cadastro entidade historico

--- src/main/java/br/com/sgee/gestaodeequipamentos/model/Historico.java
+++ src/main/java/br/com/sgee/gestaodeequipamentos/model/Historico.java
@@ -1,7 +1,23 @@
 package br.com.sgee.gestaodeequipamentos.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
 
+import java.util.Date;
+
+@Data
 @Entity
+@Table(name = "historico_emprestimo")
 public class Historico {
+    @Id
+    private int id;
+
+    @JoinColumn(name = "id_equipamentos")
+    private int idEquipamento;
+    @JoinColumn(name ="id_funcionario")
+    private int idFuncionario;
+    @Column(name ="descricao", length = 255)
+    private String descricao;
+    @Column(name ="status")
+    private String status;
 }
