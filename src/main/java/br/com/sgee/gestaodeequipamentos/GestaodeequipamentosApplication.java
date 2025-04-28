e79f3e14a732ec09b3fe94f6ff0ffe3830641cfe
markinog <marcussla9@gmail.com>
2025-04-28 11:31:53 -0300

    first commit

---
+++ src/main/java/br/com/sgee/gestaodeequipamentos/GestaodeequipamentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.sgee.gestaodeequipamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaodeequipamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaodeequipamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sgee/gestaodeequipamentos/GestaodeequipamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sgee.gestaodeequipamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaodeequipamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
