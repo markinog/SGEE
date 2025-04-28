232240e54d1289c038405de03d018400f5f1acfa
igor.viotto <igorcviotto@gmail.com>
2025-04-28 15:32:57 -0300

    Adicao do Emprestimo entity

--- src/main/java/br/com/sgee/gestaodeequipamentos/service/EmprestimoService.java
+++ src/main/java/br/com/sgee/gestaodeequipamentos/service/EmprestimoService.java
@@ -1,4 +1,19 @@
 package br.com.sgee.gestaodeequipamentos.service;
 
+import br.com.sgee.gestaodeequipamentos.model.Emprestimo;
+import br.com.sgee.gestaodeequipamentos.model.Funcionario;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class EmprestimoService {
+
+    public Emprestimo criarEmprestimo(Integer funcionarioId){
+
+
+    }
+
 }
+
